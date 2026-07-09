258f5cff85f2ac05ea134590117f87a76b329150
Farhan-bit-pc <mohdfarhan.uae@gmail.com>
2026-07-09 15:51:38 +0400

    Modify data types for product date fields
    Changed data types for prd_start_dt and prd_end_dt from datetime to date.

--- script/silver/create_table.sql
+++ script/silver/create_table.sql
@@ -14,12 +14,13 @@ create table silver.crm_cust_info(
 if object_id ('silver.crm_prd_info', 'U') is not null drop table silver.crm_prd_info
 create table silver.crm_prd_info(
 	prd_id int, 
+	cat_id nvarchar(50), 
 	prd_key	nvarchar(50), 
 	prd_nm nvarchar(50), 
 	prd_cost int, 
 	prd_line nvarchar(50), 
-	prd_start_dt datetime, 
-	prd_end_dt datetime,
+	prd_start_dt date, 
+	prd_end_dt date,
 	dwh_create_date datetime2 default getdate()
 ); 
 
